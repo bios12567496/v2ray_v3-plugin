55652b05021e4558a45fd5f3a6fc85a1dc37b7e4
bios12567496 <bios12567496@users.noreply.github.com>
2019-07-01 09:46:13 +0800

    Update handlerServiceClient.go

--- client/handlerServiceClient.go
+++ client/handlerServiceClient.go
@@ -3,8 +3,8 @@ package client
 import (
 	"context"
 	"fmt"
-	"github.com/rico93/v2ray-sspanel-v3-mod_Uim-plugin/model"
-	"github.com/rico93/v2ray-sspanel-v3-mod_Uim-plugin/utility"
+	"github.com/bios12567496/v2ray-sspanel-v3-mod_Uim-plugin/model"
+	"github.com/bios12567496/v2ray-sspanel-v3-mod_Uim-plugin/utility"
 	"google.golang.org/grpc"
 	"strings"
 	"v2ray.com/core"
